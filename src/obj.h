27399b5f879428c3e8033dd7303708ee6fa6efb1
ashpynx <alierensimsir00@@gmail.com>
2025-08-24 00:54:36 +0300

    commit

--- src/obj.h
+++ src/obj.h
@@ -19,5 +19,27 @@ typedef struct
 
 }GameVector;
 
+typedef enum 
+{
+    
+    TYPE_FRAME,
+    TYPE_BUTTON,
+    TYPE_TEXT,
+    TYPE_MOVABLE,
+    TYPE_FIXED
+
+}objtype;
+
+typedef struct 
+{
+    
+    GameVector Position,Size;
+    long Id;
+    long parentId;          /*-1 for none*/
+    objtype type;
+
+}CoreObj;  /*Core Materials of a physical object, rather than an ui object.*/
+
+
 
 #endif
